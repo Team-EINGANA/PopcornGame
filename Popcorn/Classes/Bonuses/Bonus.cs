efb0e9125e439a511988ed653343441f5100d487
MartinShishkov <martin.shishkov@hotmail.com>
2014-10-13 14:07:15 +0300

    Popcorn Base - Classes, Interfaces, Diagram

---
+++ Popcorn/Classes/Bonuses/Bonus.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Popcorn
+{
+    public abstract class Bonus : GameObject, IMovable, IBonus, ITimeoutable
+    {
+    }
+}
---
+++ Popcorn/Classes/GameObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Popcorn
+{
+    public abstract class GameObject : IDrawable, ICollidable
+    {
+        protected Position position;
+        protected Size size;
+        protected string imagePath;
+    }
+}
